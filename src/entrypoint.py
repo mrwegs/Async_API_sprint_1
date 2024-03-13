e13cb1465b3a5f8c10f9f16c5b867394c9691377
Evgeni Storozhenko <w.evgenius@gmail.com>
2024-03-13 15:34:50 +0300

    upd: add lifespan contextmanager

--- src/entrypoint.py
+++ src/entrypoint.py
@@ -1,18 +1,39 @@
+from contextlib import asynccontextmanager
 import logging
 
 import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
+from redis.asyncio import Redis
+from elasticsearch import AsyncElasticsearch
 
 from src.core import config
 from src.core.logger import LOGGING
+from db import redis
+from db import elastic
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    redis.redis = Redis(host=config.REDIS_HOST, port=config.REDIS_PORT)
+    elastic.es = AsyncElasticsearch(hosts=[f'{config.ELASTIC_HOST}:{config.ELASTIC_PORT}'])
+    yield
+    await redis.redis.close()
+    await elastic.es.close()
 
 app = FastAPI(
     title=config.PROJECT_NAME,
+    lifespan=lifespan,
     docs_url='/api/openapi',
     openapi_url='/api/openapi.json',
     default_response_class=ORJSONResponse,
 )
+
+
+@app.get('/')
+def index():
+    return {'hello': 'world'}
+
 
 if __name__ == '__main__':
     uvicorn.run(

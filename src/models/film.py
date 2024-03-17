ce57934666be93ca2b35157beea4dc196fd2338c
Evgeni Storozhenko <w.evgenius@gmail.com>
2024-03-17 13:24:33 +0300

    Add new models FilmDetails

--- src/models/film.py
+++ src/models/film.py
@@ -1,5 +1,5 @@
 import orjson
-from pydantic import BaseModel
+from pydantic import UUID4, BaseModel
 
 
 def orjson_dumps(v, *, default):
@@ -14,3 +14,35 @@ class Film(BaseModel):
     class Config:
         json_loads = orjson.loads
         json_dumps = orjson_dumps
+
+
+class Model(BaseModel):
+    id: UUID4
+    name: str
+
+    def __eq__(self, __other: 'Model') -> bool:
+        return self.id == __other.id
+
+    def __hash__(self) -> int:
+        return self.id.int
+
+
+class Actor(Model):
+    """Класс для описания актера"""
+
+
+class Writer(Model):
+    """Класс для описания сценариста"""
+
+
+class FilmDetails(BaseModel):
+    id: str
+    imdb_rating: float | None
+    genre: list[str]
+    title: str
+    description: str | None
+    director: list[str]
+    actors: list[Actor]
+    writers: list[Writer]
+    actors_names: list[str]
+    writers_names: list[str]

58b21526c5fd7237d98310ff7e87fe574e3f4ac6
Evgeni Storozhenko <w.evgenius@gmail.com>
2024-03-17 12:45:14 +0300

    add: responses for APIRouter

--- src/api/v1/films.py
+++ src/api/v1/films.py
@@ -12,7 +12,10 @@ from pydantic import BaseModel
 from src.services.film import get_film_service, FilmService
 from src.api.v1.params import Params
 
-router = APIRouter()
+router = APIRouter(
+    tags=['films'],
+    responses={HTTPStatus.NOT_FOUND: {'description': 'Not found'}},
+)
 
 
 class FilmResponse(BaseModel):
@@ -21,7 +24,7 @@ class FilmResponse(BaseModel):
 
 
 @router.get('/search')
-async def get_films_by_title(
+async def search_films(
         title_query: Annotated[str | None, Query(min_length=3)],
         params: Params = Depends(),
         film_service: FilmService = Depends(get_film_service)
@@ -29,14 +32,19 @@ async def get_films_by_title(
 
     films = await film_service.get_films_list(params=params, title_query=title_query)
 
+    if not films:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND)
+
     return [FilmResponse(id=film.id, title=film.title) for film in films]
 
 
 @router.get('/{film_id}', response_model=FilmResponse)
 async def film_details(film_id: str, film_service: FilmService = Depends(get_film_service)) -> FilmResponse:
+
     film = await film_service.get_by_id(film_id)
+
     if not film:
-        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='film not found')
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND)
 
     return FilmResponse(id=film.id, title=film.title)
 
@@ -49,5 +57,8 @@ async def get_films(
 ) -> list[FilmResponse]:
 
     films = await film_service.get_films_list(params=params, genre=genre)
+
+    if not films:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND)
 
     return [FilmResponse(id=film.id, title=film.title) for film in films]

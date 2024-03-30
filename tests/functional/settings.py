a8c07801a62f79798fa3f40cf20e15bcb5fcbf22
Evgeni Storozhenko <w.evgenius@gmail.com>
2024-03-30 15:26:14 +0300

    upd: update settings and mappings

--- tests/functional/settings.py
+++ tests/functional/settings.py
@@ -1,18 +1,57 @@
+from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+from tests.functional.testdata.es_mapping import (
+    GENRES,
+    GENRES_SETTINGS,
+    MOVIES,
+    MOVIES_SETTINGS,
+    PERSONS,
+    PERSONS_SETTINGS
+)
 
-class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8', extra='ignore')
 
-    redis_host: str = ...
-    redis_port: int = 6379
-    redis_db: int = 2
+class EnvSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_file='tests/functional/.env', env_file_encoding='utf-8', extra='ignore')
 
-    elastic_host: str = ...
-    elastic_port: int = 9200
 
-    async_api_host: str = ...
-    async_api_port: int = 8000
+class ESIndexSettings(EnvSettings):
+    movies_index_name: str = Field(default=...)
+    genres_index_name: str = Field(default=...)
+    persons_index_name: str = Field(default=...)
+
+    index_list: list[str] = [str(movies_index_name), str(persons_index_name), str(genres_index_name)]
+
+    movies_mappings: dict = MOVIES
+    genres_mappings: dict = GENRES
+    persons_mappings: dict = PERSONS
+    movies_settings: dict = MOVIES_SETTINGS
+    genres_settings: dict = GENRES_SETTINGS
+    persons_settings: dict = PERSONS_SETTINGS
+
+
+class Settings(EnvSettings):
+    redis_host: str = Field(default=...)
+    redis_port: int = Field(default=...)
+    redis_db: int = Field(default=...)
+
+    elastic_host: str = Field(default=...)
+    elastic_port: int = Field(default=...)
+
+    async_api_host: str = Field(default=...)
+    async_api_port: int = Field(default=...)
+
+    service_url: str = f'http://{async_api_host}:{async_api_port}'
+
 
 
 settings = Settings()
+
+es_settings = ESIndexSettings()
+
+dev_settings = Settings(
+    redis_host='127.0.0.1',
+    elastic_host='127.0.0.1',
+    async_api_host='l127.0.0.1',
+    service_url='http://127.0.0.1:80'
+)

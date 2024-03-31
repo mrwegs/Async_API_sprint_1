a4b5604cb10e27d34faaa41ef74dc803264c51e5
Evgeni Storozhenko <w.evgenius@gmail.com>
2024-03-31 13:39:32 +0300

    upd: remove person_data duplicate fixtures

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -80,7 +80,6 @@ async def es_delete_data(es):
 
 @pytest_asyncio.fixture(scope="session")
 def film_data():
-  
     index_name = es_settings.movies_index_name
 
 # 1. Генерируем данные для ES
@@ -111,30 +110,6 @@ def film_data():
 
     return bulk_query
 
-@pytest_asyncio.fixture(scope="session")
-def person_data():
-    index_name = es_settings.persons_index_name
-
-# 1. Генерируем данные для ES
-    es_data = [{
-        'uuid': str(uuid.uuid4()),
-        'full_name': 'Anna De Armas',
-        'films': [
-            {'uuid': 'fb111f22-121e-44a7-b78f-b19191810fbf', 'title': 'Blade Runner', 'imdb_rating': 8.0, 'roles': ['actor']},
-            {'uuid': 'caf76c67-c0fe-477e-8766-3ab3ff2574b5', 'title': 'Knives Out', 'imdb_rating': 7.9, 'roles': ['actor']},
-        ],
-        # 'created_at': datetime.datetime.now().isoformat(),
-        # 'updated_at': datetime.datetime.now().isoformat(),
-        # 'film_work_type': 'movie'
-    } for _ in range(60)]
-
-    bulk_query: list[dict] = []
-    for row in es_data:
-        data = {'_index': index_name, '_id': row['uuid']}
-        data.update({'_source': row})
-        bulk_query.append(data)
-
-    return bulk_query
 
 @pytest_asyncio.fixture(scope="session")
 def person_data():
